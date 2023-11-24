3d15033d3feb12006c69fbe0ec2e4e94045b9829
Iulian <petruiulian01@gmail.com>
2023-11-24 13:58:00 +0200

    new features

---
+++ src/components/FinishScreen.js
@@ -0,0 +1,19 @@
+function FinishScreen({ points, maxPossiblePoints, highscore, dispatch }) {
+    const percentage = Math.round(points / maxPossiblePoints * 100);
+    let emoji;
+    if (percentage === 100) emoji = '🤩';
+    if (percentage >= 80 && percentage < 100) emoji = '😎';
+    if (percentage >= 60 && percentage < 80) emoji = '😀';
+    if (percentage >= 40 && percentage < 60) emoji = '😐';
+    if (percentage >= 20 && percentage < 40) emoji = '😕';
+    if (percentage < 20) emoji = '😱';
+    return (
+        <>
+            <p className="result">{emoji} You scored <strong>{points}</strong> out of {maxPossiblePoints} ({percentage}%)</p>
+            <p className="highscore">(Highscore: {highscore} points)</p>
+            <button className="btn btn-ui" onClick={() => dispatch({ type: 'restart' })}>Restart</button>
+        </>
+    )
+}
+
+export default FinishScreen
